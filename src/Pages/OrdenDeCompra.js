bd4d10c7e1968d6c5554bc1d0a706352430ee0ee
AlanGolubeff <alangolu99@gmail.com>
2023-05-18 22:15:18 -0300

    react-pf

--- src/Pages/OrdenDeCompra.js
+++ src/Pages/OrdenDeCompra.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { addDoc, collection, getFirestore, serverTimestamp, doc, updateDoc } from "firebase/firestore";
 import { useCart } from 'react-use-cart';
-import { Form, Button, Modal } from 'react-bootstrap';
-
+import { Form, Button, Modal, Container } from 'react-bootstrap';
+import "./OrdenDeCompra.css"
 
 import { useThemeHook } from '../GlobalComponents/ThemeProvider';
 export default function OrdenDeCompra() {
@@ -49,54 +49,55 @@ export default function OrdenDeCompra() {
     })*/
     return (
         <>
-
-            <div className='buyForm'>
-                <Form onSubmit={(e) => { e.preventDefault(); sendOrder() }}>
-                    <input type="text" value={name} name="nameForm" id="nameForm" placeholder="name"
-                        onChange={(e) => setName(e.target.value)} required
-                    />
-                    <input type="text" value={email} name="emailForm" id="emailForm" placeholder='email@email.com'
-                        onChange={(e) => setEmail(e.target.value)} required
-                    />
-                    <input type="text" value={phone} name="phoneForm" id="phoneForm" placeholder='+54(0XX)XXX-XX-XX'
-                        onChange={(e) => setPhone(e.target.value)} required
-                    />
-                    <input type="text" value={address} name="addressForm" id="addressForm" required placeholder='Avenida Siempre Viva 123'
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
+            <Container>
+                <div className='buyForm'>
+                    <Form onSubmit={(e) => { e.preventDefault(); sendOrder() }}>
+                        <input type="text" value={name} name="nameForm" id="nameForm" placeholder="name"
+                            onChange={(e) => setName(e.target.value)} required
+                        />
+                        <input type="text" value={email} name="emailForm" id="emailForm" placeholder='email@email.com'
+                            onChange={(e) => setEmail(e.target.value)} required
+                        />
+                        <input type="text" value={phone} name="phoneForm" id="phoneForm" placeholder='+54(0XX)XXX-XX-XX'
+                            onChange={(e) => setPhone(e.target.value)} required
+                        />
+                        <input type="text" value={address} name="addressForm" id="addressForm" required placeholder='Avenida Siempre Viva 123'
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                        {
+                            items.length === 0 ?
+                                <Button variant="contained" disabled >Enviar</Button> :
+                                <Button onClick={handleOpen} type="submit" variant="contained" >Enviar</Button>
+                        }
+                    </Form>
                     {
-                        items.length === 0 ?
-                            <Button variant="contained" disabled >Enviar</Button> :
-                            <Button onClick={handleOpen} type="submit" variant="contained" >Enviar</Button>
+                        orderCode === "" ? null :
+                            <Modal
+                                show={open}
+                                size="lg"
+                                aria-labelledby="contained-modal-title-vcenter"
+                                centered
+                            >
+                                <Modal.Header closeButton>
+                                    <Modal.Title id="contained-modal-title-vcenter">
+                                        Modal heading
+                                    </Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <h2>{name}</h2>
+                                    <h2>{email}</h2>
+                                    <h2>{phone}</h2>
+                                    <h2>{address}</h2>
+                                    <h2>{cartTotal}</h2>
+                                    <h2>{orderCode}</h2>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button onClick={handleClose} >Close</Button>
+                                </Modal.Footer>
+                            </Modal>
                     }
-                </Form>
-                {
-                    orderCode === "" ? null :
-                        <Modal
-                            show={open}
-                            size="lg"
-                            aria-labelledby="contained-modal-title-vcenter"
-                            centered
-                        >
-                            <Modal.Header closeButton>
-                                <Modal.Title id="contained-modal-title-vcenter">
-                                    Modal heading
-                                </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <h2>{name}</h2>
-                                <h2>{email}</h2>
-                                <h2>{phone}</h2>
-                                <h2>{address}</h2>
-                                <h2>{cartTotal}</h2>
-                                <h2>{orderCode}</h2>
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button onClick={handleClose} >Close</Button>
-                            </Modal.Footer>
-                        </Modal>
-                }
-            </div>
+                </div>
+            </Container>
         </>
     )
 }
